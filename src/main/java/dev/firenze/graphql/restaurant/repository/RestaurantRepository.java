c21637dcce54d7776aa3e3e9128e63df4a81a06c
Tiziano Pessa <tiziano.pessa@register.it>
2022-05-11 12:11:57 +0200

    fix: workaround con query a causa di un bug hibernate (https://hibernate.atlassian.net/browse/HHH-15142)

--- src/main/java/dev/firenze/graphql/restaurant/repository/RestaurantRepository.java
+++ src/main/java/dev/firenze/graphql/restaurant/repository/RestaurantRepository.java
@@ -2,6 +2,8 @@ package dev.firenze.graphql.restaurant.repository;
 
 import dev.firenze.graphql.restaurant.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ import java.util.List;
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 
-  List<Restaurant> findByCityLikeIgnoreCase(String city);
+  @Query("SELECT r FROM Restaurant r WHERE r.city like %:city%")
+  List<Restaurant> findByCityLikeIgnoreCase(@Param("city") String city);
 }
